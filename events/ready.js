22bad69f775505f3cb7f6a650b859438874142fd
giahuyyphung <169174586+giahuyyphung@users.noreply.github.com>
2024-06-01 22:05:48 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -6,7 +6,7 @@ module.exports = (client) => {
   const activities = [`Giveaways in ${client.guilds.cache.size} guilds`,"g!help",`over ${client.users.cache.size} users!`];
   setInterval(() => {
     let activity = activities[Math.floor(Math.random() * activities.length)];
-    client.user.setActivity("Source by ZeroDiscord", { type: "WATCHING" });
+    client.user.setActivity("Source by test", { type: "CUSTOM" });
   }, 20000);
 
 };
