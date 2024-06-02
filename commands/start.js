ea34721448795cf448f6f5b748381c8201deb0ac
giahuyyphung <169174586+giahuyyphung@users.noreply.github.com>
2024-06-02 08:24:15 +0700

    Update start.js

--- commands/start.js
+++ commands/start.js
@@ -52,8 +52,8 @@ exports.run = async (client, message, args) => {
         messages: {
             giveaway: (client.config.everyoneMention ? "<@&1080877156588060712>\n\n" : "")+"<a:hc_sparkle:1239157985809403936> **Giveaways Đã Bắt Đầu** <a:hc_sparkle:1239157985809403936>\nHosted by: <@" + message.author.id + ">",
             giveawayEnded: (client.config.everyoneMention ? "@everyone\n\n" : "")+"<a:hc_sparkle:1239157985809403936> **Giveaways Đã Kết Thúc** <a:hc_sparkle:1239157985809403936>\nHosted by: <@" + message.author.id + ">",
-            timeRemaining: "Time remaining: **{duration}**!",
-            inviteToParticipate: "React with <a:hg_giveaways:1212044076585848883> to participate!",
+            timeRemaining: "<a:hc_quay:1210414481503559701> Countdown: **{duration}**!",
+            inviteToParticipate: "<a:hc_muiten:1239143693479579658> React with 🎉 to participate!",
             winMessage: "<a:hg_party:1082275579979649054> | Congratulations, {winners} won the **{prize}** Giveaways!",
             embedFooter: "Giveaways",
             noWinner: "Giveaway cancelled, no valid participations.",
